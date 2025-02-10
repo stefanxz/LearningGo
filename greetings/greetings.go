65b4f27bcacf6ae916b4c79502bb92ffc0091dd7
Stefan Avram <stefanavram55@gmail.com>
2025-02-10 16:42:18 +0200

    Added comment explaining exportable functions

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,8 @@
 package greetings
 
-import "fmt"
-
-// Hello is a function that takes a string variable "name" that returns a string type
+import (
+	"fmt"
+) // Hello is a function that takes a string variable "name" that returns a string type
 func Hello(name string) string {
 	message := fmt.Sprintf("Hi, %v. Welcome!", name) // Sprintf is a string formatting function, where %v is placeholder
 	return message
--- hello/hello.go
+++ hello/hello.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-	message := greetings.Hello("Gladys")
+	message := greetings.Hello("Gladys") // 'Hello' is a functioned wrote with a capital first letter -> acts like a public function in Java/ can be exported and used in other packages.
 
 	fmt.Println(message)
 }
