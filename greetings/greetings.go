f6fa19831b0607e60a5611bee74566dfb2b1f023
Stefan Avram <stefanavram55@gmail.com>
2025-02-10 17:14:00 +0200

    Added explanaions for error handling code. Functions that handle errors can return a tuple of (returnValue, error), and have the possiibbility ot also return nil, when the error is encountered/cought

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,15 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
-) // Hello is a function that takes a string variable "name" that returns a string type
-func Hello(name string) string {
+)
+
+// Hello is a function that takes a string variable "name" that returns a string type
+func Hello(name string) (string, error) { // now function can return error string type and error type
+	if name == "" {
+		return "", errors.New("this name was empty") // errors are never to be capitalized by convention
+	}
 	message := fmt.Sprintf("Hi, %v. Welcome!", name) // Sprintf is a string formatting function, where %v is placeholder
-	return message
+	return message, nil                              // return nil("") for when error is encountered
 }
