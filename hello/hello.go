1c6fcd9011f6eea56c09983a529e34c2393a2ff0
Stefan Avram <stefanavram55@gmail.com>
2025-02-10 17:11:58 +0200

    Learned about error handling and loggers; loggers are actually prety useful for debugging

--- hello/hello.go
+++ hello/hello.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"fmt"
-
 	"LearningGo/greetings"
+	"fmt"
+	"log" //log is just a console.log that also prints the exact time at which the message is logged
 )
 
 func main() {
 
-	message := greetings.Hello("Gladys") // 'Hello' is a functioned wrote with a capital first letter -> acts like a public function in Java/ can be exported and used in other packages.
+	log.SetPrefix("greetings: ") // instead of having a prefix as a date, it tells you where the error happened
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("") // 'Hello' is a functioned wrote with a capital first letter -> acts like a public function in Java/ can be exported and used in other packages.
+	//since greetings.Hello can now return an error value as well, it s return value is assigned to a (message, err) tuple
+	if err != nil { // if the error variable gets a value, it means that an error was encountered -> err != nil
+		log.Fatal(err) // fatal error, exists program -> everything below it is unreachable
+	}
+
+	//everything goes well, just print message.
 
 	fmt.Println(message)
 }
+
+//I can create more loggers just for seeing what errors go on and where
+//appLogger := log.New(os.Stdout, "APP: ", log.Ldate|log.Ltime|log.Lshortfile)
+//dbLogger := log.New(os.Stdout, "DB: ", log.Ldate|log.Ltime|log.Lshortfile)
+//netLogger := log.New(os.Stdout, "NET: ", log.Ldate|log.Ltime|log.Lshortfile)
